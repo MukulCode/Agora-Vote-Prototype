523f2def8850a4d30d23710151f1407a61a56ff4
Mukul Kumar <mukul.2121@gmail.com>
2019-03-22 14:21:58 +0530

    New code is here

---
+++ app/src/main/java/com/example/agoravoteprototype/Signin.java
@@ -0,0 +1,25 @@
+package com.example.agoravoteprototype;
+
+import android.content.Intent;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+
+public class Signin extends AppCompatActivity {
+    Button signinbtn;
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_signin);
+        signinbtn=(Button) findViewById(R.id.signinbtn);
+        signinbtn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent2= new Intent(Signin.this, Election1.class);
+                startActivity(intent2);
+
+            }
+        });
+    }
+}
