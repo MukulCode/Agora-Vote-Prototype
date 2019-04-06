c4f2f5c144c89f3b8344b512df47d8e5102ef7f7
Mukul Kumar <mukul.2121@gmail.com>
2019-04-06 09:31:03 +0530

    Splash screen icon rotation is included

--- app/src/main/java/com/example/agoravoteprototype/SplashScreen.java
+++ app/src/main/java/com/example/agoravoteprototype/SplashScreen.java
@@ -4,6 +4,9 @@ import android.content.Intent;
 import android.os.Handler;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.animation.Animation;
+import android.view.animation.AnimationUtils;
+import android.widget.ImageView;
 
 public class SplashScreen extends AppCompatActivity {
 
@@ -11,18 +14,30 @@ public class SplashScreen extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_splash_screen);
+        final ImageView iv=(ImageView) findViewById(R.id.imageView2);
+        final Animation an = AnimationUtils.loadAnimation(getBaseContext(),R.anim.rotate);
+        final Animation an2 = AnimationUtils.loadAnimation(getBaseContext(),R.anim.abc_fade_out);
 
-        new Handler().postDelayed(new Runnable() {
+        iv.startAnimation(an);
+        an.setAnimationListener(new Animation.AnimationListener() {
+            @Override
+            public void onAnimationStart(Animation animation) {
+
+            }
 
             @Override
-            public void run() {
-
-                Intent i = new Intent(SplashScreen.this, before_signup.class);
-                startActivity(i);
-
+            public void onAnimationEnd(Animation animation) {
+                iv.startAnimation(an2);
                 finish();
+                Intent i= new Intent(SplashScreen.this,before_signup.class);
+                startActivity(i);
             }
-        }, 3000);
+
+            @Override
+            public void onAnimationRepeat(Animation animation) {
+
+            }
+        });
 
     }
 }
